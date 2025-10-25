655aa2ac7c35f3a9aaf25a994e3f7455b9a9cf32
MrinalSontakke <mrinal.rv.sontakke@gmail.com>
2025-10-25 18:55:18 -0400

    adding the set prescription function

--- ui.h
+++ ui.h
@@ -7,6 +7,7 @@ void printDivider();             // Prints divider for layout
 void printWelcomeMessage();      // Prints program header
 void showLowPillWarning(int remaining); // Shows low-pill alert
 void displayStatus();            // Displays current pill status
+void setPrescription();          // saves the original prescription from the user. 
 void showMainMenu();             // Main menu and user interaction
 
 #endif
