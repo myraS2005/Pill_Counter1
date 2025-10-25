b9c86b576eda56ab8f02e9b48bbe17c238c1aebf
arshia-singh <arshiasingh0311@gmail.com>
2025-10-25 16:22:15 -0400

    created time_manager files

--- time_manager.h
+++ time_manager.h
@@ -1,1 +1,33 @@
+#ifndef TIME_MANAGER_H
+#define TIME_MANAGER_H
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+
+enum Limits {
+	LIMIT_DOSAGES = 100
+};
+
+typedef struct DoseTime {
+	int day;
+	int month;
+	int year;
+	bool used;
+
+} dose_time_t;
+
+
+//extern dose_time_t allDoseTimes[LIMIT_DOSAGES];
+
+
+
+
+void setMaxDosage(int dose);
+
+bool doseTakenToday();
+
+int newDose();
+
+
+#endif 
 
